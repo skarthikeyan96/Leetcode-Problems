43db0a76dd55de3e0a20e07dcdaf7bb58fd3ebf4
Karthikeyan <imkarthikeyans@gmail.com>
2022-06-01 04:07:16 +0000

    Sync LeetCode submission - Contains Duplicate (javascript)

--- problems/contains_duplicate/solution.js
+++ problems/contains_duplicate/solution.js
@@ -3,7 +3,11 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    return nums.filter((num,pos) => {   
-        return nums.indexOf(num) === pos
-    }).length === nums.length ? false: true 
+    let array = nums.filter((value, index) => index !== nums.indexOf(value));
+    
+    
+   console.log(array)
+    
+   return array.length === 0 ? false : true
 };
+
