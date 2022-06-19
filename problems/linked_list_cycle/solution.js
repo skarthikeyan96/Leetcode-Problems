f792491ef16f88b7e914a2987830af7af90ad692
Karthikeyan <imkarthikeyans@gmail.com>
2022-06-19 08:44:19 +0000

    Sync LeetCode submission - Linked List Cycle (javascript)

---
+++ problems/linked_list_cycle/solution.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    
+    
+    if(head === null || head.next === null){
+        return false
+    }
+    
+   let previous = head;
+    let current = head
+    
+    let hasLinkedListCycle = false;
+    
+    
+    while(previous !==null && previous.next !== null){
+        
+     previous = previous.next.next;
+    current = current.next;
+    if(previous === current){
+       return true
+    }
+       
+        
+    }
+    
+    return false
+   
+};
