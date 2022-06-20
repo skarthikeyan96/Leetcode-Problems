b61af1b48385cef162f53f0945061d7a1610f691
Karthikeyan <imkarthikeyans@gmail.com>
2022-06-20 18:12:17 +0000

    Sync LeetCode submission - Climbing Stairs (javascript)

---
+++ problems/climbing_stairs/solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+  
+    let arr = [];
+    arr[0] = 1;
+    arr[1] = 1;
+    arr[2] = 2;
+    
+    for(let i=3;i<=n;i++){
+        arr[i] = arr[i-1] + arr[i-2]
+    }
+    return arr[n]
+};
