46bd12efcdbb30206ab760e894a198c0167152e0
Karthikeyan <imkarthikeyans@gmail.com>
2022-06-24 09:00:25 +0000

    Sync LeetCode submission - Kth Largest Element in an Array (javascript)

--- problems/kth_largest_element_in_an_array/solution.js
+++ problems/kth_largest_element_in_an_array/solution.js
@@ -4,6 +4,5 @@
  * @return {number}
  */
 var findKthLargest = function(nums, k) {
-    console.log(nums.sort(function(a, b){return b-a}))
     return(nums.sort(function(a, b){return b-a})[k-1])
 };
