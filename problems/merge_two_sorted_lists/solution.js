922a179465ac9e8e40f117f7c59529c8fbd1e012
Karthikeyan <imkarthikeyans@gmail.com>
2022-08-04 07:08:33 +0000

    Sync LeetCode submission - Merge Two Sorted Lists (javascript)

--- problems/merge_two_sorted_lists/solution.js
+++ problems/merge_two_sorted_lists/solution.js
@@ -20,53 +20,12 @@ var mergeTwoLists = function(list1, list2) {
     if(list2 === null && list1 !== null) return list1;
 
     
-//     let first = list1;
-    
-//     while(list1.next !== null){
-//         list1 = list1.next;
-//     }
-//     // console.log(list1)
-//     // connect list1 and list 2
-//     list1.next = list2
-    
-//     // sort the list 
-//     // console.log(first)
-    
-//     let pointerOne = first;
-    
-//     while(pointerOne !== null){
-        
-//         let current = pointerOne;
-        
-//         while(current.next !== null){
-            
-//             if(current.val > current.next.val){
-//                 let t = current.next.val;
-//                 current.next.val = current.val;
-//                 current.val = t;
-//             }else{
-//                 console.log(current.val);
-//                 console.log(current.next.val)
-//             }
-//             current = current.next
-//         }
-        
-//         pointerOne = pointerOne.next;
-//     }
-    
-   
-    
-//     return(first)
-    
     
     let temp1 = list1;
     let temp2 = list2;
     let array = []
     
-    let resultList = null;
-//     console.log(temp1.val)
-//     console.log(temp2.val)
-    
+
     let head = new ListNode();
     let tail = head;
     
@@ -93,43 +52,6 @@ var mergeTwoLists = function(list1, list2) {
     tail.next = temp1 == null ? temp2 : temp1
     
     return(head.next)
-    // // console.log(array)
-    // return constructList(array.concat(traverseList(temp2)))
+   
 };
 
-
-const traverseList = (temp2) => {
-    let array = []
-    
-    while(temp2 !== null){
-        array.push(temp2.val) 
-        temp2 = temp2.next
-    }
-    
-    return array
-}
-
-const constructList = (arr) => {
-    let list = new ListNode();
-    
-    list.val = arr[0];
-    list.next = null;
-    
-    
-    let temp1 = list;
-    
-    for(let i=1;i<arr.length;i++){
-        let temp = new ListNode();
-        temp.val = arr[i];
-        temp.next = null;
-        
-        // console.log(temp)
-        
-        temp1.next = temp
-        temp1 = temp1.next
-        // console.log(temp1)
-    }
-    
-        return(list)
-
-}
