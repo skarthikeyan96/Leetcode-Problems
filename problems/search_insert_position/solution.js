8500256965fedf26df51d0ae88e0200b3b2a5e2e
Karthikeyan <imkarthikeyans@gmail.com>
2022-08-26 02:12:28 +0000

    Sync LeetCode submission - Search Insert Position (javascript)

---
+++ problems/search_insert_position/solution.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+
+    nums.push(target);
+    
+    nums.sort((a,b) => a - b)
+    
+    return nums.indexOf(target)
+
+};
