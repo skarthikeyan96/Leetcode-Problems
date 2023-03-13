36ca0cab7478cc718e07195050c394a00563498e
Karthikeyan <imkarthikeyans@gmail.com>
2023-03-13 16:39:39 +0000

    Sync LeetCode submission - Binary Search (javascript)

--- problems/binary_search/solution.js
+++ problems/binary_search/solution.js
@@ -4,10 +4,5 @@
  * @return {number}
  */
 var search = function(nums, target) {
-  for(let i=0;i<nums.length;i++){
-      if(nums[i] === target){
-          return i
-      }
-  }
-    return -1
+    return nums.indexOf(target)
 };
